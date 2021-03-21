d5ab25b2d3c1a5580523b112e5279b58241f3c32
Sagun Sharma <63535221+sagunsharma@users.noreply.github.com>
2021-03-21 11:54:03 +0530

    Last Change
    fb Intregration

--- src/main/java/com/facebook/auth/FbLoginAuthApplication.java
+++ src/main/java/com/facebook/auth/FbLoginAuthApplication.java
@@ -1,14 +1,30 @@
 package com.facebook.auth;
 
+import java.security.Principal;
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@SpringBootApplication
-@EnableOAuth2Sso
 @ComponentScan(basePackages = "com.fb.auth.ctrl")
-public class FbLoginAuthApplication {
+@SpringBootApplication
+@Configuration
+@EnableOAuth2Sso
+public class FbLoginAuthApplication extends WebSecurityConfigurerAdapter  {
+	
+	 protected void configure(HttpSecurity http) throws Exception {
+	      http
+	        .csrf().disable();
+	  }
 
 	public static void main(String[] args) {
 		SpringApplication.run(FbLoginAuthApplication.class, args);
--- src/main/java/com/fb/auth/ctrl/MainCtrl.java
+++ src/main/java/com/fb/auth/ctrl/MainCtrl.java
@@ -3,20 +3,39 @@ package com.fb.auth.ctrl;
 import java.security.Principal;
 import java.util.Map;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.oauth2.provider.OAuth2Authentication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-@RestController
+@Controller
 public class MainCtrl 
 {
-	@RequestMapping("/")
-	public String userAuth(Principal principal)
+	@RequestMapping(value = "/user", method = RequestMethod.GET)
+		public String userAuth(Principal principal)
+		 {
+			System.out.println("Main ctrl");
+			 OAuth2Authentication auth=(OAuth2Authentication) principal;
+			 if(auth==null)
+			 {
+				 return "errorpage.html";
+			 }
+			 else
+			 {
+			 Map<String, Object> user=(Map<String, Object>) auth.getUserAuthentication().getDetails();
+			System.out.println("Welcome: "+user.get("name"));
+			 return "sucess.html";
+			 }
+		 }  
+	@RequestMapping("/welcome")
+	public String welcome()
 	 {
-		System.out.println("Main ctrl");
-		 OAuth2Authentication auth=(OAuth2Authentication) principal;
-		 Map<String, Object> user=(Map<String, Object>) auth.getUserAuthentication().getDetails();
-		System.out.println("user details"+user);
-		 return "Welcome: "+user.get("name");
+		System.out.println("Welcome");
+		 return "Welcome";
 		 
 	 }
+	
 }
